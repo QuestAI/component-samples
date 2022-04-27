db8b5a814d6ee6c187cdb091a24a0cff45396d72
Sudhir Subramanian <sudhir@quest.ai>
2022-04-27 12:38:03 -0700

    Latest samples

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import MoonBaseApp from "./moonbase/MoonbaseApp";
 
 function App() {
   return (
-    <MoonBaseApp />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/moonbase/*" element={<MoonBaseApp />} />
+        <Route path="*" element={<div />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/moonbase/MoonbaseApp.js
+++ src/moonbase/MoonbaseApp.js
@@ -4,14 +4,15 @@ import { StyledEngineProvider } from '@mui/material/styles';
 import styled from '@emotion/styled';
 
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 
-import moonbaseLightTheme from "./MoonbaseDesignSystemLightTheme";
-import Dashboard from "./Dashboard";
-
+import moonbaseLightTheme from "./dashboard/MoonbaseDesignSystemLightTheme";
+import Dashboard from "./dashboard/Dashboard";
+import NavbarPage from "./dashboard/NavbarPage";
+import FeedbackBarPage from "./dashboard/FeedbackBarPage";
+import ProductsPage from "./payment/ProductsPage";
 
 const MainDiv = styled("div")({
   width: "100vw",
@@ -27,14 +28,13 @@ function MoonbaseApp() {
     <ThemeProvider theme={moonbaseLightTheme}>
       <StyledEngineProvider injectFirst>
         <MainDiv>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/navbar" element={<Dashboard />} />
-              <Route path="/feedbackbar" element={<Dashboard />} />
-              <Route path="/" element={<Dashboard />} />
-            </Routes>
-          </BrowserRouter>
+          <Routes>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="navbar" element={<NavbarPage />} />
+            <Route path="feedbackbar" element={<FeedbackBarPage />} />
+            <Route path="payment" element={<ProductsPage />} />
+            <Route path="*" element={<NavbarPage />} />
+          </Routes>
         </MainDiv>
       </StyledEngineProvider>
     </ThemeProvider >
